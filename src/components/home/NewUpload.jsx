c1375842ff4c87f880e7dd5148a2abaca78b70ab
Mobin <contact@mobin.dev>
2025-12-01 06:53:03 +0600

    fixed responsive issues

--- src/components/home/NewUpload.jsx
+++ src/components/home/NewUpload.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function NewUpload() {
   return (
-    <div className="p-3 bg-gray rounded-md flex items-center gap-3 flex-wrap">
+    <div className="p-3 bg-gray rounded-md flex items-center gap-3 ">
       <Image
         src={"/profile.png"}
         className="rounded-full static"
@@ -14,15 +14,21 @@ export default function NewUpload() {
       />
 
       <input
-        className="bg-light-gray-50 rounded-full px-7 pl-8 h-full  py-3 flex-1"
+        className="bg-light-gray-50 rounded-full px-7 pl-2 md:pl-8 h-full  py-3 flex-1"
         type="string"
         placeholder="Whats One your Mind, Raihan?"
       />
 
       <div className="flex items-center  gap-4">
-        <Video size={34} className="text-red-500 cursor-pointer" />
-        <ImageIcon size={30} className="text-green-500 cursor-pointer" />
-        <ListVideo size={30} className="text-pink-600 cursor-pointer" />
+        <Video
+          size={34}
+          className="text-red-500 cursor-pointer hidden sm:block"
+        />
+        <ImageIcon size={30} className="text-green-500 cursor-pointer " />
+        <ListVideo
+          size={30}
+          className="text-pink-600 cursor-pointer hidden sm:block"
+        />
       </div>
     </div>
   );
